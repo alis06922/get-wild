5fee7947b5931be3ecf4ca5f0db9bd61512a1f47
Saleem Ali <saleema@clavax.com>
2025-03-24 21:12:35 +0530

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 (function($){
+    document.addEventListener("DOMContentLoaded", function() {
+        var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazyload-bg"));
+      
+        if ("IntersectionObserver" in window) {
+          let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+              if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                lazyBackgroundObserver.unobserve(entry.target);
+              }
+            });
+          });
+      
+          lazyBackgrounds.forEach(function(lazyBackground) {
+            lazyBackgroundObserver.observe(lazyBackground);
+          });
+        }
+      });
+    
+      
 	$(document).ready(function() {	
+
 
     
 
